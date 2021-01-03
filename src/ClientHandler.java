086448a5e8e03592fd529cec1558d24253f2a31f
Konrad <konrad1jasinski@gmail.com>
2021-01-03 19:59:37 +0100

    only last client gets start message

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -4,12 +4,16 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Random;
+
+
 
 public class ClientHandler implements Runnable {
     private Socket client;
     private BufferedReader in;
     private PrintWriter out;
     private ArrayList<ClientHandler> clients;
+
 
     public ClientHandler(Socket clientSocket, ArrayList<ClientHandler> clients) throws IOException {
         this.client = clientSocket;
@@ -18,11 +22,28 @@ public class ClientHandler implements Runnable {
         out = new PrintWriter(client.getOutputStream(), true);
 
 
+
+
+
+
     }
 
     @Override
     public void run() {
         int errorCounter=0;
+        ArrayList<String> numbers = new ArrayList<>();
+        numbers.add("1");
+        numbers.add("2");
+        numbers.add("3");
+        numbers.add("4");
+        Random random = new Random();
+        String first = numbers.get(random.nextInt(numbers.size()));
+        numbers.remove(first);
+        String second = numbers.get(random.nextInt(numbers.size()));
+        numbers.remove(second);
+        String third = numbers.get(random.nextInt(numbers.size()));
+        numbers.remove(third);
+        String fourth = numbers.get(0);
         try {
             while (true) {
                 if (errorCounter == 100) break;
@@ -33,7 +54,9 @@ public class ClientHandler implements Runnable {
                 } else out.println("OK");
 
                 if (clients.size() == 4){
-                    out.println("START "+ );
+                    out.println("START "+ first+" "+ first+" "+second+" "+third+" "+fourth );
+
+
                 }
             }
         } catch (IOException e) {
@@ -49,5 +72,11 @@ public class ClientHandler implements Runnable {
             }
         }
 
+    }
+
+    private void outToAll(String msg){
+        for (ClientHandler aClient : clients){
+            aClient.out.println(msg);
+        }
     }
 }
